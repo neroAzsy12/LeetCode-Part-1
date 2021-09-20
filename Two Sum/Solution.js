4f3bcff9a5ea06da22e0bb6a8a998f18d548bd53
neroAzsy12 <azael.zamora12@gmail.com>
2021-09-20 00:11:13 -0700

    Add files via upload

---
+++ Two Sum/Solution.js
@@ -0,0 +1,12 @@
+function twoSum(nums, target) {
+    const compliment = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (compliment.has(target - nums[i])) {
+            return [i, compliment.get(target - nums[i])];
+        }
+        compliment.set(nums[i], i);
+    }
+    
+    return [];
+};
