9f3dd861019205be44c63873955ed79e932f3c92
neroAzsy12 <azael.zamora12@gmail.com>
2021-09-20 00:07:07 -0700

    Add files via upload

---
+++ Two Sum/Solution.ts
@@ -0,0 +1,12 @@
+function twoSum(nums: number[], target: number): number[] {
+    const compliment: Map<number, number> = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (compliment.has(target - nums[i])) {
+            return [i, compliment.get(target - nums[i])!];
+        }
+        compliment.set(nums[i], i);
+    }
+    
+    return [];
+};
