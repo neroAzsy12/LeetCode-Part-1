ab90188d9d36c11b8be3111aa0ff4690a2c67d34
neroAzsy12 <azael.zamora12@gmail.com>
2021-09-20 00:12:03 -0700

    Update Solution.ts

--- Two Sum/Solution.ts
+++ Two Sum/Solution.ts
@@ -3,7 +3,7 @@ function twoSum(nums: number[], target: number): number[] {
 
     for (let i = 0; i < nums.length; i++) {
         if (compliment.has(target - nums[i])) {
-            return [i, compliment.get(target - nums[i])!];
+            return [i, compliment.get(target - nums[i])];
         }
         compliment.set(nums[i], i);
     }
