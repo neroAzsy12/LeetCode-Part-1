091cb272b99a9b62c4e393baac26cfed053f056d
neroAzsy12 <azael.zamora12@gmail.com>
2021-09-29 01:37:19 -0700

    Update Solution.java

--- Group Anagrams/Solution.java
+++ Group Anagrams/Solution.java
@@ -1,18 +1,18 @@
 import java.util.*;
 public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // Maps sorted anagram string to a list of anagrams
+        // Maps the pairs <sorted-anagram-string, list of matching anagram(s)>
         HashMap<String, List<String>> anagrams_grouped = new HashMap<>();
 
-        for (String s : strs) {
-            char[] alphabet = new char[26]; // char array, to keep track of each letter occurrence
+        for (String s : strs) {             // iterate through all the strings in strs
+            char[] alphabet = new char[26]; // char array, to keep track of each letter occurrence in the string
 
             for (char c : s.toCharArray()) {
                 alphabet[c - 'a']++;        // increment the index position of the char, by 1
             }
 
             String key = new String(alphabet);                      // create a string based on the alphabet char array
-            anagrams_grouped.putIfAbsent(key, new ArrayList<>());   // if the string is not in map, add the string with a new ArrayList
+            anagrams_grouped.putIfAbsent(key, new ArrayList<>());   // if the string of the sorted anagram is not in the map, add the string with a new ArrayList
             anagrams_grouped.get(key).add(s);                       // add the anagram to the corresponding sorted anagram key
         }
 
