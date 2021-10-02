14b703caf75a0740e9745d4c28a4f0c38f56041c
neroAzsy12 <azael.zamora12@gmail.com>
2021-10-02 01:09:34 -0700

    Update Solution.java

--- Search a 2D Matrix/Solution.java
+++ Search a 2D Matrix/Solution.java
@@ -1,4 +1,5 @@
 class Solution {
+    // time complexity: O(log(nm)), since matrix is m x n
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix.length == 0 || matrix[0].length == 0) {
             return false;
@@ -7,19 +8,22 @@ class Solution {
         int left = 0;
         int right = matrix.length - 1;
         
+        // do binary search to find the row in which the target value could potentially appear in
         while (left <= right) {
             int mid = (left + right) / 2;
         
-            if (matrix[mid][0] < target) {
+            if (matrix[mid][0] < target) {          // if target is greater, ignore left half
                 left = mid + 1;
-            } else if (matrix[mid][0] > target) {
+            } else if (matrix[mid][0] > target) {   // if target is smaller, ignore right half
                 right = mid - 1;
             } else {
-                return true;
+                return true;    // returns true if matrix[mid][0] == target
             }
         }
         
-        int row = right;
+        int row = right;    // row is initialized based on, which row target could potentially appear
+        
+        // do binary search to find if the target value appears in the row, that was found by doing binary search
         if (row >= 0 && row < matrix.length) {
             left = 0;
             right = matrix[0].length - 1;
@@ -32,11 +36,11 @@ class Solution {
                 } else if (matrix[row][mid] > target) {
                     right = mid - 1;
                 } else {
-                    return true;
+                    return true;    // returns true if matrix[row][mid] == target
                 }
             }
         }
         
-        return false;
+        return false;   // returns false, since the matrix does not contain target
     }
 }
