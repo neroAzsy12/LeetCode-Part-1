00f739d8cca9985b569560020a23f225e9c96e11
neroAzsy12 <azael.zamora12@gmail.com>
2021-10-05 00:25:50 -0700

    Add files via upload

---
+++ Valid Parentheses/Solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+	// time complexity: O(n)
+	public boolean isValid(String s) {
+        Stack<Character> chars = new Stack<Character>();	// use a stack for LIFO order
+		for(int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			switch(c) {
+				case '(':
+				case '{':
+				case '[':
+					chars.add(c);		// add the open parentheses to the stack chars
+					break;
+				
+				case ')':
+					if(chars.isEmpty() || chars.pop() != '(')	
+						return false;	// if the last char added to chars is not the open paren, or is empty return false
+					break;
+				
+				case '}':
+					if(chars.isEmpty() || chars.pop() != '{') 
+						return false;	// if the last char added to chars is not the open braket, or is empty return false
+					break;
+				
+				case ']':
+					if(chars.isEmpty() || chars.pop() != '[')
+						return false;	// if the last char added to chars is not the open braket, or is empty return false
+					break;
+			}
+		}
+		
+		return chars.isEmpty();	// if chars is empty, string s is a valid string of parentheses, else it is not valid format
+    }
+}
